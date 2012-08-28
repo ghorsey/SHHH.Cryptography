5b6cabd468746e4b063408cbd2d4d553f2b3419b
ghorsey <geoff@shhhinnovations.com>
2012-08-27 17:26:26 -0700

    updated nuget batch file

--- code/src/nuget.bat
+++ code/src/nuget.bat
@@ -1,2 +1,1 @@
-.nuget\nuget.exe install SHHH.Infrastructure.NHibernate\packages.config -OutputDirectory packages
-.nuget\nuget.exe install SHHH.Infrastructure.NHibernate.Mvc\packages.config -OutputDirectory packages
+.nuget\nuget.exe install SHHH.Cryptography.Tests\packages.config -OutputDirectory packages
